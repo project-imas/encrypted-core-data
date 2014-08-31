21d88dbac9e94221b52c80bc4bd717f30ba45de5
Rich Hodgkins <rhodgkins@gmail.com>
2014-08-31 17:33:01 +0100

    Added relational object graph and models with different relationships

---
+++ exampleProjects/IncrementalStore/ClassModels/ISDChildA.m
@@ -0,0 +1,16 @@
+//
+//  ISDChildA.m
+//  Incremental Store
+//
+//  Created by Richard Hodgkins on 31/08/2014.
+//  Copyright (c) 2014 Caleb Davenport. All rights reserved.
+//
+
+#import "ISDChildA.h"
+
+
+@implementation ISDChildA
+
+@dynamic attributeA;
+
+@end
