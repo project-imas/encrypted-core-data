4e764a99f680b6e8836ea9244ee6dd30ede29fb0
Daniel Broad <daniel@dorada.org>
2015-12-22 10:37:59 +0000

    Added Unit Tests for Migration and SUM: expressions

--- exampleProjects/IncrementalStore/Tests/IncrementalStoreTests.m
+++ exampleProjects/IncrementalStore/Tests/IncrementalStoreTests.m
@@ -81,6 +81,7 @@
     for (NSUInteger i = 0; i < count; i++) {
         id object = [NSEntityDescription insertNewObjectForEntityForName:@"User" inManagedObjectContext:context];
         [object setValue:[NSString stringWithFormat:@"%lu username",(unsigned long)i] forKey:@"name"];
+        [object setValue:[NSNumber numberWithInteger:i] forKey:@"age"];
     }
     error = nil;
     BOOL save = [context save:&error];
@@ -811,6 +812,42 @@
            XCTAssertFalse(count == NSNotFound, @"Error with fetch: %@", error);
            XCTAssertEqual(count, [expectedCount unsignedIntegerValue], @"Incorrect fetch count");
     }];
+}
+
+-(void)test_sumExpression {
+    
+    [self createUsers:10];
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    
+    NSEntityDescription *entityDescription = [NSEntityDescription
+                                              entityForName:@"User" inManagedObjectContext:context];
+    
+    [fetchRequest setEntity:entityDescription];
+    [fetchRequest setResultType:NSDictionaryResultType];
+    
+    NSExpression *expression = [NSExpression expressionForKeyPath:@"age"];
+    NSExpression *sumExpression = [NSExpression expressionForFunction:@"sum:"
+                                                            arguments:[NSArray arrayWithObject:expression]];
+    NSExpressionDescription *sumExpressionDescription = [[NSExpressionDescription alloc] init];
+    [sumExpressionDescription setName:@"total age"];
+    [sumExpressionDescription setExpression:sumExpression];
+    [sumExpressionDescription setExpressionResultType:NSInteger64AttributeType];
+    
+    [fetchRequest setPropertiesToFetch:@[sumExpressionDescription]];
+    
+    NSError *error = nil;
+    NSArray *age = [context executeFetchRequest:fetchRequest error:&error];
+    XCTAssertNil(error, @"Error running query %@",error);
+    XCTAssertNotNil(age, @"No Results");
+    XCTAssertEqual(age.count, 1, @"Incorrect fetch count");
+    
+    NSDictionary *results = [age firstObject];
+    
+    NSInteger expectedAge = 45;
+    
+    NSNumber *totalAge = [results objectForKey:@"total age"];
+    XCTAssertEqual(totalAge.integerValue, expectedAge, @"Incorrect total %ld expected %ld",(long)totalAge.integerValue,(long)expectedAge);
 }
 
 @end
